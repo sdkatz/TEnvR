0c25bc2c6f0b3fbdfaee87b498b5e548d95add9e
Sam Katz <43495056+sdkatz@users.noreply.github.com>
2026-02-02 22:01:38 -0500

    Update FTMS_FormulaAssignment.m
    Updated the code using matlabs built in fix function. it just replaced some find function with logic.

--- FTMS_FormulaAssignment.m
+++ FTMS_FormulaAssignment.m
@@ -65,7 +65,7 @@ if strcmpi(config.Ionization_Mode,'negative')
         DBE = 1+(((2*FormulaCombinations(format.Row_C,:))-(FormulaCombinations(format.Row_H,:)+1)+FormulaCombinations(format.Row_N,:)+FormulaCombinations(format.Row_P,:))/2);
     end
     
-    Index_Refined=find(floor(DBE) == DBE & DBE>=config.DBE_min & DBE<=config.DBE_max); % Find combinations with a whole AND positive DBE value
+    Index_Refined=floor(DBE) == DBE & DBE>=config.DBE_min & DBE<=config.DBE_max; % Find combinations with a whole AND positive DBE value
     FormulaCombinations_Refined = FormulaCombinations(:,Index_Refined);
     
     ExactMass = FormulaCombinations_Refined(format.Row_C,:)*config.Mass_12C + FormulaCombinations_Refined(format.Row_H,:)*config.Mass_1H + FormulaCombinations_Refined(format.Row_N,:)*config.Mass_14N +...
@@ -75,7 +75,7 @@ if strcmpi(config.Ionization_Mode,'negative')
 elseif strcmpi(config.Ionization_Mode,'positive')
     % Refine Formula Combinations by removing formulas with both Na and K
     NaK=FormulaCombinations(format.Row_K,:)+FormulaCombinations(format.Row_Na,:);
-    index_refined1=find(NaK <= 1); % Find combinations with Na+K<=1
+    index_refined1=NaK <= 1; % Find combinations with Na+K<=1
     FormulaCombinations_Refined1 = FormulaCombinations(:,index_refined1);
     
     % Refine Formula Combinations by DBE
@@ -87,7 +87,7 @@ elseif strcmpi(config.Ionization_Mode,'positive')
         DBE = 1+(((2*FormulaCombinations_Refined1(format.Row_C,:))-(FormulaCombinations_Refined1(format.Row_H,:)-1+FormulaCombinations_Refined1(format.Row_K,:)+FormulaCombinations_Refined1(format.Row_Na,:))+FormulaCombinations_Refined1(format.Row_N,:)+FormulaCombinations_Refined1(format.Row_P,:))/2);
     end
     
-    index_refined2=find(floor(DBE) == DBE & DBE>=config.DBE_min & DBE<=config.DBE_max); %Find combinations with a whole AND positive DBE value
+    index_refined2=floor(DBE) == DBE & DBE>=config.DBE_min & DBE<=config.DBE_max; %Find combinations with a whole AND positive DBE value
     FormulaCombinations_Refined = FormulaCombinations_Refined1(:,index_refined2);
     
     ExactMass = FormulaCombinations_Refined(format.Row_C,:)*config.Mass_12C + FormulaCombinations_Refined(format.Row_H,:)*config.Mass_1H + FormulaCombinations_Refined(format.Row_N,:)*config.Mass_14N +...
@@ -101,7 +101,7 @@ for i = 1:NumberOfPeaks
     C_max = floor(mz(i)/config.Mass_12C);
     
     if C_max <= max(FormulaCombinations_Refined(format.Row_C,:)) || C_max <= config.C_min
-        ;       
+               
     else
         C_min_new = max(FormulaCombinations_Refined(format.Row_C,:));
         
@@ -117,7 +117,7 @@ for i = 1:NumberOfPeaks
                 DBE_New = 1+(((2*FormulaCombinations_New(format.Row_C,:))-(FormulaCombinations_New(format.Row_H,:)+1)+FormulaCombinations_New(format.Row_N,:)+FormulaCombinations_New(format.Row_P,:))/2);
             end
             
-            Index_Refined_New=find(floor(DBE_New) == DBE_New & DBE_New>=config.DBE_min & DBE_New<=config.DBE_max); % Find combinations with a whole AND positive DBE value
+            Index_Refined_New=floor(DBE_New) == DBE_New & DBE_New>=config.DBE_min & DBE_New<=config.DBE_max; % Find combinations with a whole AND positive DBE value
             FormulaCombinations_New_Refined = FormulaCombinations_New(:,Index_Refined_New);
             
             ExactMass_New = FormulaCombinations_New_Refined(format.Row_C,:)*config.Mass_12C + FormulaCombinations_New_Refined(format.Row_H,:)*config.Mass_1H + FormulaCombinations_New_Refined(format.Row_N,:)*config.Mass_14N +...
@@ -128,7 +128,7 @@ for i = 1:NumberOfPeaks
 
             % Refine Formula Combinations by removing formulas with both Na and K
             NaK_New=FormulaCombinations_New(format.Row_K,:)+FormulaCombinations_New(format.Row_Na,:);
-            index_refined1_New=find(NaK_New <= 1); % Find combinations with Na+K<=1
+            index_refined1_New=NaK_New <= 1; % Find combinations with Na+K<=1
             FormulaCombinations_New_Refined1 = FormulaCombinations_New(:,index_refined1_New);
 
             % Refine Formula Combinations by DBE
@@ -140,7 +140,7 @@ for i = 1:NumberOfPeaks
                 DBE_New = 1+(((2*FormulaCombinations_New_Refined1(format.Row_C,:))-(FormulaCombinations_New_Refined1(format.Row_H,:)-1+FormulaCombinations_New_Refined1(format.Row_K,:)+FormulaCombinations_New_Refined1(format.Row_Na,:))+FormulaCombinations_New_Refined1(format.Row_N,:)+FormulaCombinations_New_Refined1(format.Row_P,:))/2);
             end
 
-            index_refined2_New=find(floor(DBE_New) == DBE_New & DBE_New>=config.DBE_min & DBE_New<=config.DBE_max); %Find combinations with a whole AND positive DBE value
+            index_refined2_New=floor(DBE_New) == DBE_New & DBE_New>=config.DBE_min & DBE_New<=config.DBE_max; %Find combinations with a whole AND positive DBE value
             FormulaCombinations_New_Refined = FormulaCombinations_New_Refined1(:,index_refined2_New);
 
             ExactMass_New = FormulaCombinations_New_Refined(format.Row_C,:)*config.Mass_12C + FormulaCombinations_New_Refined(format.Row_H,:)*config.Mass_1H + FormulaCombinations_New_Refined(format.Row_N,:)*config.Mass_14N +...
